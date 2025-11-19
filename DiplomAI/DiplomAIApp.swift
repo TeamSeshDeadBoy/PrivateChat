34197502b903667c233f94b4b3e9f38c205ff87b
StepanLebedev <stepan_leb@mail.ru>
2025-11-19 17:11:43 +0300

    Initial Commit

---
+++ DiplomAI/DiplomAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiplomAIApp.swift
+//  DiplomAI
+//
+//  Created by Stepan Lebedev on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct DiplomAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
